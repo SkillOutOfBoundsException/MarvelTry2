b7fbd4fd030d68293971a75a9205c150620b8ecf
Ivan Carcamo <ivandres73@unitec.edu>
2016-11-15 22:44:20 -0600

    Quitado comentarios estrobosos
    EL login agarra el enter :+1:

--- src/objects/Logic.java
+++ src/objects/Logic.java
@@ -1,16 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package objects;
 
 import java.util.ArrayList;
 
-/**
- *
- * @author Diegu7
- */
 public class Logic {
     
     public ArrayList<Usuario> arrUsu;
--- src/objects/Usuario.java
+++ src/objects/Usuario.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package objects;
 
-/**
- *
- * @author Diegu7
- */
 public class Usuario {
     
     private String nombre;
@@ -52,6 +44,5 @@ public class Usuario {
     public int getPoints(){
         return puntos;
     }
-    
     
 }
