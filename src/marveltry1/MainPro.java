39e60f8d200c5421cdbb5ec84da1c247a8b22c51
Ivan Carcamo <ivandres73@unitec.edu>
2016-11-17 13:41:12 -0600

    Hoy si papa

--- src/marveltry1/MainPro.java
+++ src/marveltry1/MainPro.java
@@ -1,25 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package marveltry1;
 
 import visual.MainFrame;
 import objects.Logic;
 
-/**
- *
- * @author Diegu7
- */ 
 public class MainPro {
     
     public static MainFrame lol = new MainFrame();
     public static Logic funcs = new Logic();
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         
         MainFrame.startFrame();
