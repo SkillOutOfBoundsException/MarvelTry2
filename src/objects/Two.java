a55bdcb46f80940c4ed2b4b6ddc7ffee22ba2dca
skilloutofboundsexception <mecayounbloque@hotmail.com>
2016-12-10 18:44:59 -0600

    two movement

--- src/objects/Two.java
+++ src/objects/Two.java
@@ -5,6 +5,9 @@
  */
 package objects;
 
+import visual.BetterButtons;
+import visual.ButtonsP;
+
 /**
  *
  * @author Diegu7
@@ -15,4 +18,67 @@ public class Two extends Ficha{
         power = 2;
     }
     
+    
+    @Override
+    public boolean selected(BetterButtons x){
+        boolean flac = false;
+        if(hero){
+            if(x.y > 0 && (ButtonsP.grid[x.x][x.y-1].ficha == null|| ButtonsP.grid[x.x][x.y-1].ficha.hero == !hero)){//up
+                ButtonsP.grid[x.x][x.y-1].setText("GLOW");
+                ButtonsP.grid[x.x][x.y-1].available = true;
+                flac = true;
+            }
+            if(x.x < 9 && (ButtonsP.grid[x.x+1][x.y].ficha == null || ButtonsP.grid[x.x+1][x.y].ficha.hero == !hero)){//right
+                ButtonsP.grid[x.x+1][x.y].setText("GLOW");
+                ButtonsP.grid[x.x+1][x.y].available = true;
+                flac = true;
+            }
+            if(x.x > 0 && (ButtonsP.grid[x.x-1][x.y].ficha == null || ButtonsP.grid[x.x-1][x.y].ficha.hero == !hero)){//left
+                ButtonsP.grid[x.x-1][x.y].setText("GLOW");
+                ButtonsP.grid[x.x-1][x.y].available = true;
+                flac = true;
+            }
+            if(x.x > 0 && x.y > 0 && (ButtonsP.grid[x.x-1][x.y-1].ficha == null || ButtonsP.grid[x.x-1][x.y-1].ficha.hero == !hero)){//diagonal left
+                ButtonsP.grid[x.x-1][x.y-1].setText("GLOW");
+                ButtonsP.grid[x.x-1][x.y-1].available = true;
+                flac = true;
+            }
+            if(x.x < 9 && x.y > 0 && (ButtonsP.grid[x.x+1][x.y-1].ficha == null || ButtonsP.grid[x.x+1][x.y-1].ficha.hero == !hero)){//diagonal right
+                ButtonsP.grid[x.x+1][x.y-1].setText("GLOW");
+                ButtonsP.grid[x.x+1][x.y-1].available = true;
+                flac = true;
+            }
+            ButtonsP.selected = flac;
+            return flac;
+        }
+        else{
+            if(x.y < 9 && (ButtonsP.grid[x.x][x.y+1].ficha == null || ButtonsP.grid[x.x][x.y+1].ficha.hero == !hero)){//down
+                ButtonsP.grid[x.x][x.y+1].setText("GLOW");
+                ButtonsP.grid[x.x][x.y+1].available = true;
+                flac = true;
+            }
+            if(x.x < 9 && (ButtonsP.grid[x.x+1][x.y].ficha == null || ButtonsP.grid[x.x+1][x.y].ficha.hero == !hero)){//right
+                ButtonsP.grid[x.x+1][x.y].setText("GLOW");
+                ButtonsP.grid[x.x+1][x.y].available = true;
+                flac = true;
+            }
+            if(x.x > 0 && (ButtonsP.grid[x.x-1][x.y].ficha == null || ButtonsP.grid[x.x-1][x.y].ficha.hero == !hero)){//left
+                ButtonsP.grid[x.x-1][x.y].setText("GLOW");
+                ButtonsP.grid[x.x-1][x.y].available = true;
+                flac = true;
+            }
+            if(x.x > 0 && x.y < 9 && (ButtonsP.grid[x.x-1][x.y+1].ficha == null || ButtonsP.grid[x.x-1][x.y+1].ficha.hero == !hero)){//diagonal left
+                ButtonsP.grid[x.x-1][x.y+1].setText("GLOW");
+                ButtonsP.grid[x.x-1][x.y+1].available = true;
+                flac = true;
+            }
+            if(x.x < 9 && x.y < 9 && (ButtonsP.grid[x.x+1][x.y+1].ficha == null || ButtonsP.grid[x.x+1][x.y+1].ficha.hero == !hero)){//diagonal right
+                ButtonsP.grid[x.x+1][x.y+1].setText("GLOW");
+                ButtonsP.grid[x.x+1][x.y+1].available = true;
+                flac = true;
+            }
+            ButtonsP.selected = flac;
+            return flac;
+        }
+    }
 }
